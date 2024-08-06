79825947806ed45d544d1b9992145c694112348b
goyang1103 <00bsj@naver.com>
2024-08-07 03:31:35 +0900

    메인페이지 헤더 및 사이드바 전역 스타일 구성

---
+++ src/common/commonForm/Header.jsx
@@ -0,0 +1,183 @@
+import styled from "styled-components";
+import Logo from "../../img/background/logo.png"
+import logosearch from "../../img/loginImg/findglass.png";
+import { Link } from "react-router-dom";
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 6vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  border-bottom: 1px solid #C8CBD9;
+`;
+
+const LeftBox = styled.div`
+  width: 14%;
+  height: 100%;
+  background-color: #f1f2f7;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RightBox = styled.div`
+  width: 86%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoBox = styled(Link)`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  border-radius: 50%;
+  background-color: #96a8de;
+  /* background-image: url(${Logo});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center; */
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const LogoTitle = styled(Link)`
+  width: 50%;
+  height: 90%;
+  padding-left: 4%;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  color: #5a67ba;
+  font-family: "Poppins-Bold", Helvetica;
+  font-size: 15px;
+  font-weight: 600;
+  white-space: nowrap;
+  text-decoration: none;
+  &:hover {
+    font-size: 16px;
+  }
+`;
+
+const SearchBox = styled.div`
+  width: 55%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SearchInput = styled.input.attrs({ type: "text" })`
+  display: flex;
+  width: 85%;
+  height: 50%;
+  padding: 2%;
+  background-color: #F6F6FB;
+  color: #1F384C;
+  font-size: 12px;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+  &:focus {
+    border: 1px solid darkgray;
+  }
+`;
+
+const Searchlogo = styled.img`
+  width: 15px;
+  height: 15px;
+  cursor: pointer;
+  position: absolute;
+  margin-left: 80%;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+
+const ToggleBox = styled.div`
+  width: 15%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+` 
+
+const Toggle = styled.div`
+  width: 68px;
+  height: 32px;
+  background-color: #78c6ff;
+  border-radius: 104px;
+  box-shadow: 0px 2.5px 6.25px #90909040;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UserBox = styled.div`
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MerchantName = styled.div`
+  width: 85px;
+  height: 33px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #1f384c;
+  font-family: "Roboto-Regular", Helvetica;
+  font-size: 13px;
+  font-weight: 400;
+  background-color: #f5f6fa;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #ebecef;
+  }
+`;
+
+const Header = () => {
+  return (
+        <HeaderContainer>
+            <LeftBox>
+                <LogoBox to="/"></LogoBox>
+                <LogoTitle to="/">신용만</LogoTitle>
+            </LeftBox>
+            <RightBox>
+              <SearchBox>
+                <SearchInput
+                  placeholder="검색어를 입력해주세요."
+                //   value={searchTerm}
+                //   onChange={(e) => setSearchTerm(e.target.value)}
+                //   onKeyPress={handleKeyPress}
+                />
+                <Searchlogo
+                  src={logosearch}
+                //   onClick={() => {
+                //     handleSearch();
+                //   }}
+                />
+              </SearchBox>
+                <ToggleBox>
+                    <Toggle></Toggle>
+                </ToggleBox>
+                <UserBox>
+                    <MerchantName>로그인</MerchantName>
+                </UserBox>
+            </RightBox>
+        </HeaderContainer>
+  );
+};
+export default Header;
---
+++ src/common/commonForm/SideBar.jsx
@@ -0,0 +1,145 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
+import styled, { css } from "styled-components";
+import {
+  IoSettingsOutline,
+  IoReaderOutline,
+  IoBarChartOutline,
+  IoChatbubbleEllipsesOutline,
+  IoNewspaperOutline,
+  IoTodayOutline,
+  IoChatboxEllipsesOutline,
+} from "react-icons/io5";
+
+const Sidebar = styled.div`
+  width: 14%;
+  height: 100%;
+  background-color: #f1f2f7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Menu = styled.div`
+  width: 53%;
+  height: 88%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+`;
+
+const activeTitleStyle = css`
+  background-color: #d8dcf3;
+  opacity: 0.8;
+  border-radius: 5px;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  height: 45px;
+  color: #082431;
+  font-family: "Poppins-Regular", Helvetica;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  line-height: 11px;
+  opacity: 0.5;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+
+const ContentsBox = styled(Link)`
+  width: 100%;
+  height: 42px;
+  padding-left: 7%;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  white-space: nowrap;
+  text-decoration: none;
+  ${(props) => props.isActive && activeTitleStyle}
+  &:hover {
+    transform: scale(1.02);
+    opacity: 0.7;
+  }
+`;
+
+const IconBox = styled.div`
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextWrapper = styled.div`
+  width: 80%;
+  color: #273240;
+  font-family: "Roboto-Regular", Helvetica;
+  font-size: 13px;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+  padding-left: 5px;
+`;
+
+const SideBar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  return (
+    <Sidebar>
+      <Menu>
+        <Title>MENU</Title>
+        <ContentsBox to="/" isActive={currentPath === "/"}>
+          <IconBox>
+            <IoBarChartOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>Dashboard</TextWrapper>
+        </ContentsBox>
+        <ContentsBox to="/notice" isActive={currentPath === "/notice"}>
+          <IconBox>
+            <IoTodayOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>공지사항</TextWrapper>
+        </ContentsBox>
+        <ContentsBox
+          to="/information"
+          isActive={currentPath === "/information"}
+        >
+          <IconBox>
+            <IoReaderOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>신용정보</TextWrapper>
+        </ContentsBox>
+        <ContentsBox to="/chat" isActive={currentPath === "/chat"}>
+          <IconBox>
+            <IoNewspaperOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>신용평가</TextWrapper>
+        </ContentsBox>
+        <ContentsBox to="/evaluation" isActive={currentPath === "/evaluation"}>
+          <IconBox>
+            <IoChatbubbleEllipsesOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>문의사항</TextWrapper>
+        </ContentsBox>
+        <Title>OTHERS</Title>
+        <ContentsBox to="/setting" isActive={currentPath === "/setting"}>
+          <IconBox>
+            <IoSettingsOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>설정</TextWrapper>
+        </ContentsBox>
+        <ContentsBox to="/help" isActive={currentPath === "/help"}>
+          <IconBox>
+            <IoChatboxEllipsesOutline size={20} color="#A6ABC8" />
+          </IconBox>
+          <TextWrapper>고객지원</TextWrapper>
+        </ContentsBox>
+      </Menu>
+    </Sidebar>
+  );
+};
+export default SideBar;
